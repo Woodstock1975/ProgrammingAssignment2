aba7afc4ed79cb5c0c90ab348c6a03a6e2707e85
Woodstock1975 <georg@georg-MacBook.(none)>
2015-02-22 17:24:08 -0500

    Version from Feb 22

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+# makeCacheMatrix creates a list of four functions set, get, setinv, getinv.
+# set sets the entries of a matrix
+# get retrieves the entries of the matrix
+# setinv defines a matrix inv in the cache. The purpose of inv is to save the inverse of a matrix so that it does not have to be re-computed.
+# getinv retrieves the matrix saved in the cache variable inv.   
 
 makeCacheMatrix <- function(X = matrix()) {
         inv <- NULL
@@ -18,7 +20,8 @@ makeCacheMatrix <- function(X = matrix()) {
 }
 
 
-## Write a short comment describing this function
+
+# cacheSolve computes the inverse of a matrix X. If there is a matrix saved in the cache variable inv, then this matrix is retrieved and output as the inverse matrix of X. Furthermore a message "getting cached inverse" is printed. If there is no matrix saved in inv, then the inverse of X is computed using "solve", and the result is saved in the variable inv.
 
 cacheSolve <- function(X, ...) {
         ## Return a matrix that is the inverse of 'X'
